592f270610fc812c501991793d6e7fd69bee6070
chrisjsimpson <chris.j.simpson@live.co.uk>
2021-10-10 05:19:49 +0100

    experiment with uwsgi dynamic worker scalling called cheaper
    see https://www.techatbloomberg.com/blog/configuring-uwsgi-production-deployment/

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-exec uwsgi --http-socket 0.0.0.0:9090 --uid nobody --gid 65534 --manage-script-name --mount /=app:app
+exec uwsgi --master --uid nobody --gid 65534 --cheaper-algo busyness --cheaper 8 --cheaper-initial 16 --cheaper-overload 1 --cheaper-step 16 --processes 500 --http-socket 0.0.0.0:9090  --manage-script-name --mount /=app:app
